d649157461c6968d327d125aa03eaa7e7cf8cadc
Giles Roadnight <giles@roadnight.name>
2016-11-10 07:04:39 +0000

    hello world server

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
 
+/// <reference path="typings/index.d.ts" />
 
-console.log("Hello World");
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+var env = require('node-env-file');
+var package = require("./package.json");
+
+if (fs.existsSync(__dirname + '/devEnvironment.env' )) {
+  env(__dirname + '/devEnvironment.env')
+}
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end(`Hello World... Server version is ${package.version}`);
+});
+
+const port = process.env.PORT;
+
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log(`Server version ${package.version} running on port ${port}`);
