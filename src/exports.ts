a1f3b90e418c4e7bbd27fe0c7d8de6099a2bde3b
Giles Roadnight <giles@roadnight.name>
2016-11-21 20:54:59 +0000

    create exports file

---
+++ src/exports.ts
@@ -0,0 +1,6 @@
+
+import * as Common from "../contracts/common";
+import * as Browser from "./browser/googleOAuthClient";
+import * as Node from "./node/exports";
+
+export { Common, Browser, Node };
