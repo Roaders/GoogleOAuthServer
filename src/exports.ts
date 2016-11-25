093f99232ff523f71a1cd7d4399b5dbda52457bf
Giles Roadnight <giles@roadnight.name>
2016-11-25 10:23:28 +0000

    create node.js and browser.js for package consumers

--- src/exports.ts
+++
@@ -1,6 +0,0 @@
-
-import * as Common from "../contracts/common";
-import * as Browser from "./browser/googleOAuthClient";
-import * as Node from "./node/exports";
-
-export { Common, Browser, Node };
